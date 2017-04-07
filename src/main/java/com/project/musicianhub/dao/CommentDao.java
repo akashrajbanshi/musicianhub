c139f9522e4fa676c9ab7a693a4a726a9683bc10
Akash Rajbanshi <akashrajbanshi@hotmail.com>
2017-04-07 01:17:37 -0600

    commiting with comment

--- src/main/java/com/project/musicianhub/dao/CommentDao.java
+++ src/main/java/com/project/musicianhub/dao/CommentDao.java
@@ -6,12 +6,39 @@ import org.hibernate.Session;
 
 import com.project.musicianhub.model.Comment;
 
+/**
+ * Dao Interface for the comments
+ * 
+ * @author Akash Rajbanshi
+ * @since 1.0
+ *
+ */
 public interface CommentDao {
 
+	/**
+	 * Adds comments to the database
+	 * 
+	 * @param comment
+	 * @return comment object
+	 */
 	public Comment addComment(Comment comment);
 
+	/**
+	 * Adds comments to the database
+	 * 
+	 * @param session
+	 *            params
+	 * @param comment
+	 *            parameter
+	 */
 	public void addComment(Session session, Comment comment);
-	
+
+	/**
+	 * Gets the music's comments from the music id
+	 * 
+	 * @param music_id
+	 * @return comment's list
+	 */
 	public List<Comment> getCommentByMusic(int music_id);
 
 }
